5293ba1c1c72246f1bebe259d5f62a2777d8d24a
IllyaMoss <50335232+qXytreXp@users.noreply.github.com>
2021-08-13 18:26:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from os import environ
 
 
 TOKEN = environ.get('TASKBOT_TOKEN')
-CHANNEL_ID = environ.get('TASKBOT_CHANNEL_ID')
-INTERVAL = environ.get('TASKBOT_INTERVAL', default=43200)
+CHANNEL_ID = int(environ.get('TASKBOT_CHANNEL_ID'))
+INTERVAL = int(environ.get('TASKBOT_INTERVAL', default=43200))
 
 PATH_TO_TASKS = 'tasks.json'
 PATH_TO_LAST_ID = 'lasttaskid.txt'
